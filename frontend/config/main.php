1a2c43f648a1586f1a772ce2e20001f95a452821
dodo <biryukwolf@yahoo.com>
2017-07-20 06:19:13 +0300

    RBAC +

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -15,11 +15,75 @@ return [
         'request' => [
             'csrfParam' => '_csrf-frontend',
         ],
+//        'user' => [
+//            'identityClass' => 'common\models\User',
+//            'enableAutoLogin' => true,
+//            'identityCookie' => ['name' => '_identity-frontend', 'httpOnly' => true],
+//        ],
         'user' => [
-            'identityClass' => 'common\models\User',
+            'identityClass' => 'budyaga\users\models\User',
             'enableAutoLogin' => true,
-            'identityCookie' => ['name' => '_identity-frontend', 'httpOnly' => true],
+            'loginUrl' => ['/login'],
         ],
+
+
+        'authClientCollection' => [
+            'class' => 'yii\authclient\Collection',
+            'clients' => [
+                'vkontakte' => [
+                    'class' => 'budyaga\users\components\oauth\VKontakte',
+                    'clientId' => 'XXX',
+                    'clientSecret' => 'XXX',
+                    'scope' => 'email'
+                ],
+                'google' => [
+                    'class' => 'budyaga\users\components\oauth\Google',
+                    'clientId' => 'XXX',
+                    'clientSecret' => 'XXX',
+                ],
+                'facebook' => [
+                    'class' => 'budyaga\users\components\oauth\Facebook',
+                    'clientId' => 'XXX',
+                    'clientSecret' => 'XXX',
+                ],
+                'github' => [
+                    'class' => 'budyaga\users\components\oauth\GitHub',
+                    'clientId' => 'XXX',
+                    'clientSecret' => 'XXX',
+                    'scope' => 'user:email, user'
+                ],
+                'linkedin' => [
+                    'class' => 'budyaga\users\components\oauth\LinkedIn',
+                    'clientId' => 'XXX',
+                    'clientSecret' => 'XXX',
+                ],
+                'live' => [
+                    'class' => 'budyaga\users\components\oauth\Live',
+                    'clientId' => 'XXX',
+                    'clientSecret' => 'XXX',
+                ],
+                'yandex' => [
+                    'class' => 'budyaga\users\components\oauth\Yandex',
+                    'clientId' => 'XXX',
+                    'clientSecret' => 'XXX',
+                ],
+                'twitter' => [
+                    'class' => 'budyaga\users\components\oauth\Twitter',
+                    'consumerKey' => 'XXX',
+                    'consumerSecret' => 'XXX',
+                ],
+            ],
+        ],
+
+
+
+
+
+
+
+
+
+
         'session' => [
             // this is the name of the session cookie used for login on the frontend
             'name' => 'advanced-frontend',
@@ -37,13 +101,46 @@ return [
             'errorAction' => 'site/error',
         ],
 
+//        'urlManager' => [
+//            'enablePrettyUrl' => true,
+//            'showScriptName' => false,
+//            'rules' => [
+//            ],
+//        ],
+
+
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
+                '/signup' => '/user/user/signup',
+                '/login' => '/user/user/login',
+                '/logout' => '/user/user/logout',
+                '/requestPasswordReset' => '/user/user/request-password-reset',
+                '/resetPassword' => '/user/user/reset-password',
+                '/profile' => '/user/user/profile',
+                '/retryConfirmEmail' => '/user/user/retry-confirm-email',
+                '/confirmEmail' => '/user/user/confirm-email',
+                '/unbind/<id:[\w\-]+>' => '/user/auth/unbind',
+                '/oauth/<authclient:[\w\-]+>' => '/user/auth/index'
             ],
         ],
-
+        'authManager' => [
+            'class' => 'yii\rbac\DbManager',
+        ],
     ],
+
+
+    'modules' => [
+        'user' => [
+            'class' => 'budyaga\users\Module',
+            'userPhotoUrl' => 'http://example.com/uploads/user/photo',
+            'userPhotoPath' => '@frontend/web/uploads/user/photo'
+        ],
+    ],
+
+
+
+
     'params' => $params,
 ];
